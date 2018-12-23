244597e9741cc68cbd903144354aef34f84246a9
vovanquang12cntt <vanquang451@gmail.com>
2018-12-23 23:43:41 +0700

    config email

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 'use strict'
+require('dotenv').load();
+require('dotenv').config();
+
 module.exports = {
 	mailer: {
 		service: 'Gmail',
 		auth:
 		{
-			user: 'tuandnd241@gmail.com',
-			pass: 'nguyenthanhtuanmkn'
+			user: process.env.EMAIL_USERNAME,
+			pass: process.env.EMAIL_PASSWORD
 		}
 	},
 	dbConnstring:'mongodb://admin:code4share@ds129484.mlab.com:29484/code4share',
 	sessionKey:'HaloCode4Share'
-
 }
